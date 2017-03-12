e6915ed682b63b0466b2e13be2dbcacc999d8365
Maciej Misztal <mmisztal1980@gmail.com>
2017-03-12 17:37:22 +0100

    #1 project added

---
+++ src/OS.Core.Api/ApiResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OS.Core.Api
+{
+    public class ApiResult
+    {
+        public Guid CorrelationId { get; set; }
+    }
+
+    public class ApiResult<T> : ApiResult
+    {        
+        public T Value { get; set; }
+    }
+        
+}
---
+++ src/OS.Core.Api/Middleware/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OS.Core.Api.Middleware
+{
+    public static class Constants
+    {
+        public static class RequestCorrelation
+        {
+            public const string RequestHeaderName = "OpenSmog-CorrelationId";
+            public const string LogPropertyName = "CorrelationId";
+        }
+    }
+}
