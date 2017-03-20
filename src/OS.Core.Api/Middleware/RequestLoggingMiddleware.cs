82a2f33607232011c44b70e2d43872167c45c909
Maciej Misztal <mmisztal1980@gmail.com>
2017-03-20 01:26:57 +0100

    request logging middleware & startup

---
+++ src/OS.Core.Api/Middleware/RequestLoggingMiddleware.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Serilog;
+using Serilog.Events;
+
+namespace OS.Core.Middleware
+{
+    public class RequestLoggingMiddleware
+    {
+        const string MessageTemplate =
+            "HTTP {RequestMethod} {RequestPath} responded {StatusCode} in {Elapsed:0.0000} ms";
+
+        private static readonly ILogger Log = Serilog.Log.ForContext<RequestLoggingMiddleware>();
+
+        readonly RequestDelegate next;
+
+        public RequestLoggingMiddleware(RequestDelegate next)
+        {
+            if (next == null) throw new ArgumentNullException(nameof(next));
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if (httpContext == null) throw new ArgumentNullException(nameof(httpContext));
+
+            var sw = Stopwatch.StartNew();
+            try
+            {
+                await next(httpContext);
+
+                var statusCode = httpContext.Response?.StatusCode;
+                var level = statusCode > 499 ? LogEventLevel.Error : LogEventLevel.Information;
+
+                var log = level == LogEventLevel.Error ? LogForErrorContext(httpContext) : Log;
+                log.Write(level, MessageTemplate, httpContext.Request.Method, httpContext.Request.Path, statusCode,
+                    sw.Elapsed.TotalMilliseconds);
+            }
+            catch (Exception ex) when (LogException(httpContext, sw, ex))
+            {
+            }
+            finally
+            {
+                sw.Stop();
+            }
+        }
+
+        static bool LogException(HttpContext httpContext, Stopwatch sw, Exception ex)
+        {
+            sw.Stop();
+
+            LogForErrorContext(httpContext)
+                .Error(ex, MessageTemplate, httpContext.Request.Method, httpContext.Request.Path, 500, sw.Elapsed.TotalMilliseconds);
+
+            return false;
+        }
+
+        private static ILogger LogForErrorContext(HttpContext httpContext)
+        {
+            var request = httpContext.Request;
+
+            var result = Log
+                .ForContext("RequestHeaders", request.Headers.ToDictionary(h => h.Key, h => h.Value.ToString()), destructureObjects: true)
+                .ForContext("RequestHost", request.Host)
+                .ForContext("RequestProtocol", request.Protocol);
+
+            if (request.HasFormContentType)
+                result = result.ForContext("RequestForm", request.Form.ToDictionary(v => v.Key, v => v.Value.ToString()));
+
+            return result;
+        }
+    }
+}
---
+++ src/OS.Core.Api/Startup.Middleware.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using OS.Core.Middleware;
+
+namespace OS.Core
+{
+    public static class StartupMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseOpenSmogMiddlewares(this IApplicationBuilder app)
+        {
+            return app
+                .UseCorrelationIdMiddleware()
+                .UseRequestLoggingMiddleware();
+        }
+
+        public static IApplicationBuilder UseCorrelationIdMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<CorrelationIdMiddleware>();
+
+            return app;
+        }
+
+        public static IApplicationBuilder UseRequestLoggingMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<RequestLoggingMiddleware>();
+
+            return app;
+        }
+    }
+}
